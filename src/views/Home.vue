07cf89b4952db01b5c266c16eb2c33b82592a9dd
Kristjan Koitla <koitlakristjan@gmail.com>
2020-09-26 19:15:10 +0300

    Replace introduction with file upload in path '/'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  Select file to upload: 
+  <form action="">
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload file" name="submit">
+  </form>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
