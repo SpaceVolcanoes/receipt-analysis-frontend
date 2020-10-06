1372f64728068943ef4845ed46b1f54f3babc347
Kristjan Kõiv <kristjankv32@gmail.com>
2020-10-06 21:44:33 +0300

    Axios works and receipt page can display data from backend.

--- src/views/Receipt.vue
+++ src/views/Receipt.vue
@@ -1,14 +1,31 @@
 <template>
-  Select file to upload MART:
-  {{ $route.params.id }}
-  <form action="">
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload file" name="submit">
-  </form>
+  <ul>
+    <li>Receipt: {{ $data.data.id }}</li>
+    <li>User: {{ $data.data.userId }}</li>
+    <li>Issuer: {{ $data.data.issuer }}</li>
+  </ul>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "Receipt"
+  name: "Receipt",
+  data() {
+    return {
+      data: {}
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8080/receipt/2")
+      .then(res => {
+        this.data = res["data"];
+      })
+      .catch(err => {
+        alert(Object.keys(err));
+        alert(err["response"]);
+      });
+  }
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   devServer: {
-    port: 8008,
-    proxy: "http://localhost:8080"
+    port: 8008
   }
 };
