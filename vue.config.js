83c8b9ed0e7c171d4670c586a4de6253e6c21fb3
Jaak Kütt <jaak.kytt@gmail.com>
2020-10-11 21:37:00 +0300

    Add proxy and poll settings for serve

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
+const { argv } = require("yargs");
+
 module.exports = {
   devServer: {
     port: 8008
   }
 };
+
+if (argv.poll) {
+  module.exports.devServer.watchOptions = {
+    poll: true
+  };
+}
+
+if (argv.proxy) {
+  module.exports.devServer.proxy = {
+    "/api/*": {
+      target: "http://127.0.0.1:8080",
+      changeOrigin: true,
+      timeout: 6000,
+      secure: false,
+      pathRewrite: {
+        "^/api": ""
+      }
+    }
+  };
+}
