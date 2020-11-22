268a23e0d37eb78939d6b13ae0ea48b4ca68e1e3
Oleg Pahhomov <oleg.pahhomov@gmail.com>
2020-11-22 23:36:06 +0200

    grading test commit

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -20,7 +20,7 @@
 
     <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start">
-        <router-link class="navbar-item" to="/">Home</router-link>
+        <router-link class="navbar-item" to="/">Home Test</router-link>
         <router-link class="navbar-item" :to="defaultCustomerUrl"
           >CustomerReceipts</router-link
         >
